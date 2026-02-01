1b116843d59e61957bd3d1cc75011f4cb0045d38
rashednq <rashed.1nq@gmail.com>
2026-02-01 17:32:18 +0300

    Add PostgreSQL support

--- database.py
+++ database.py
@@ -1,12 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./task_manager.db"
+DATABASE_URL = "postgresql://postgres:YvreaXnxuNBeYNJIQgNHpdctyNxCycxN@postgres.railway.internal:5432/railway"
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
